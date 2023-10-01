51243aa9ece5c25a5897496d1c0b11d59aea2efb
AkiraTsugikuni17 <146665209+AkiraTsugikuni17@users.noreply.github.com>
2023-10-01 15:51:48 -0300

    Add files via upload

---
+++ src/Js/One Piece.js
@@ -0,0 +1,27 @@
+const botoes = document.querySelectorAll(".botao");
+
+const personagens = document.querySelectorAll(".personagem")
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        desselecionarBotao();
+
+        botao.classList.add("selecionado");
+
+        desselecionarpersonagem(indice);    
+})
+})
+
+function desselecionarpersonagem(indice) {
+    const personagemSelecionado = document.querySelector('.personagem.selecionado');
+    personagemSelecionado.classList.remove("selecionado");
+
+    personagens[indice].classList.add("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
